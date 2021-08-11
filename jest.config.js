df714f73b0ffc336535d488fccd62e590468c063
Luciano Tavernard <lucianotavernard@gmail.com>
2021-08-11 12:00:54 -0300

    chore: configure absolute path in jest setup

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  moduleNameMapper: {
+    '@/(.+)$': '<rootDir>/src/$1',
+  },
 }
